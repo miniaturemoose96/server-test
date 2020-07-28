dbdae4822f12e7f966be398000ef1b2ad7f20850
Alejandro <alejandro.bernal@urbantxt.com>
2020-07-28 12:11:37 -0700

    Latest Push, journal endpoints and user sign up working, still working on authentications

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const { json } = require('body-parser');
 // set dependencies 
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const cors = require('cors');
+const mongooseUniqueValidator = require('mongoose-unique-validator');
+
+// Token
+const accessTokenSecret = process.env.JWT_TOKEN;
 
 // set port and express application
 const PORT = 8888;
 const app = express();
 
 // set up middleware, help parse JSON
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // connect to mongodb atlas
-mongoose.connect('mongodb+srv://alejandro-dev1:minimoose96@cluster0.m5pfu.mongodb.net/test-database?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Successful or unsuccesful
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
     console.log('Connection Successful.');
 });
 
@@ -29,49 +39,152 @@ const journalSchema = new mongoose.Schema({
     decrip: String
 });
 
-// Create a new journal
-// just pass in schema
+// creates new schema for user sign up
+const userSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String, unique: true, required: [true, 'Field Required!'], index: true },
+    password: { type: String, required: [true, 'Field Required'], index: true }
+    // Have one more, maybe array of journals created
+}, { timestamps: true });
+
+userSchema.plugin(mongooseUniqueValidator, { message: 'already in use.' });
+
+
+// using the schema we have defined, create a new Journal
 const Journal = mongoose.model('Journal', journalSchema);
 
-// test entry
-// Our Schema Works
-// const entry = new Journal({
-//     id: "1",
-//     title: "Captain's Log",
-//     decrip: "Some star trek log reference."
-// });
+// Using new schema, create a new Person
+const Person = mongoose.model('Person', userSchema);
 
-// setting REST api endpoints
-// First we set a get
-app.get("/hello", (req, res) => {
-    res.send("HELLO WORLD!");
-});
+/** 
+ * ===============================
+ *      JOURNAL Endpoints
+ * ===============================
+*/
 
 // endpoint for saving new blogs
 app.post('/add-journal', (req, res) => {
     const journalData = req.body;
-    console.log(journalData);
-    res.send(journalData)
-    // const newJournal = new Journal(journalData);
-    
-    // // save the journal
-    // newJournal.save((err, document) => {
-    //     if(err){
-    //         res.status(500).send({
-    //             err: err,
-    //             message: 'an error occured.'
-    //         })
-    //     }
+    const newEntry = new Journal(journalData);
 
-    //     res.status(200).json(document);
-    // })
-})
+    // save the journal
+    newEntry.save((err, document) => {
+        if (err) {
+            res.status(500).send({
+                err: err,
+                message: 'an error occured.'
+            });
+        }
+        res.status(200).send({
+            message: 'Journal Saved!',
+            status: 200,
+            data: document
+        });
+    });
+});
 
 
 // endpoint for retirieving
-// app.get('/retrieve-journals', (req, res) =>{
+app.get('/view-journals', (req, res) => {
+    Journal.find((err, documents) => {
+        if (err) {
+            res.status(500).send({
+                err: err,
+                message: 'No journals are found.'
+            });
+        }
+        console.log(documents);
+        res.status(200).json(documents);
+    });
+});
 
-// });
+
+/** 
+ * ===============================
+ *      Create USER Endpoints
+ * ===============================
+*/
+
+// User post
+app.post('/add-user', (req, res) => {
+    // get data
+    let userData = req.body;
+
+    // take the password and use bcrypt to hash it
+    bcrypt.hash(userData.password, 10).then((hash) => {
+        saveUser(hash);
+    }).catch((err) => {
+        if (err) {
+            res.status(500).send({
+                err: err,
+                message: 'Error occured'
+            });
+        }
+    })
+
+    function saveUser(hash) {
+        userData.password = hash;
+        const newUser = new Person(userData);
+       
+        newUser.save((err, document) => {
+            if (err) {
+                res.status(500).send({
+                    err: err,
+                    message: 'Error occured'
+                });
+            }else{
+                /*
+                * Generate JWT for each user
+                */
+                jwt.sign({
+                    email: document.email
+                }, accessTokenSecret, (err, accessToken) => {
+                    if (err) {
+                        res.status(500).send({
+                            err: err,
+                            message: 'Cannot generate token'
+                        });
+                    } else {
+                        res.status(200).send({
+                            message: 'User Created!',
+                            status: 200,
+                            data: document,
+                            token: accessToken
+                        });
+                    }
+                });
+            }
+
+        });
+    }
+
+    // create new model instance
+    // const newUser = new Person(userData);
+
+
+});
+
+// User Get
+app.get('/auth', (req, res) => {
+    // first pull out hashed pasword using the email
+    const user = req.body;
+
+    Person.findOne(user.email, (err, document) => {
+        if (err) {
+            res.status(500).send({
+                message: 'No user found with that email'
+            });
+        }
+        console.log(document);
+    });
+    // once you have the email you want to use the bcryp compare method
+    bcrypt.compare()
+
+
+    // once you have th result of the campre method 1. handle error "invalid password" 2. handle success gen a jwt token for the new session
+});
+
 
 // set server to listen on the port given
 app.listen(PORT, () => {
